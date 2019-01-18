dcbea230ee91579c434e6b5cc9a5d094b37b7307
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2019-01-19 03:11:52 +0330

    fixed

--- src/tools/jwt.ts
+++ src/tools/jwt.ts
@@ -1,6 +1,6 @@
 import * as JWT from "jsonwebtoken";
 export function enCode(data: any) {
-    return JWT.sign(data, process.env.APP_SECRET);
+    return JWT.sign(data, process.env.APP_SECRET,{ expiresIn: '30d' });
 }
 export function deCode(token: string): any {
     // return process.env.APP_SECRET;
