82463429fad813a136e0bcc9a2a05697ac723dad
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2019-01-23 11:30:28 +0330

    SMS

--- src/tools/sms.ts
+++ src/tools/sms.ts
@@ -1,17 +1,28 @@
-import * as Request from "request-promise";
+// import * as Request from "request-promise";
+import * as  Kavenegar from "kavenegar";
+
+// tslint:disable:typedef
+
+const kavenegarApi = Kavenegar.KavenegarApi({
+    apikey: process.env.SMS_KEY
+});
 
 export class SendSms {
     token: number;
     template: string = "";
     async verify(mobile: number, code: number): Promise<boolean> {
-
-        return Request.post(process.env.SMS_KEY).then((r) => {
-            console.log("rrrrrrrrrrrr 1");
-            return true;
-        }).catch(e => {
-            // console.log("eeeeeee",);
-            throw new Error(e);
+        return await kavenegarApi.VerifyLookup({
+            receptor: mobile,
+            token: code,
+            template: "password"
         });
+        // return Request.post(process.env.SMS_KEY).then((r) => {
+        //     console.log("rrrrrrrrrrrr 1");
+        //     return true;
+        // }).catch(e => {
+        //     // console.log("eeeeeee",);
+        //     throw new Error(e);
+        // });
     }
 
 }
@@ -23,8 +34,8 @@ export function random(length: number) {
     if (length < 1) {
         throw new Error("Length must be at least 1 , generate-sms-verification-code/index.js")
     }
-    let possible = '0123456789'
-    let string = ''
+    let possible = "0123456789";
+    let string = "";
     for (let i = 0; i < length; i++) {
         string += possible.charAt(Math.floor(Math.random() * possible.length))
     }
